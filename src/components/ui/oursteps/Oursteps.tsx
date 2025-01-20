435f365e65063535d6af854aa4f03dfa6aa01202
alaagonovo <alaa@gonovo.com>
2025-01-20 12:37:06 +0200

    end software development page except steps section

--- src/components/ui/oursteps/Oursteps.tsx
+++ src/components/ui/oursteps/Oursteps.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Badge from "../common/Badge";
 
 import Step1 from "./Step1";
-import Step2 from "./Step2";
-import Step3 from "./Step3";
-import Step4 from "./Step4";
+// import Step2 from "./Step2";
+// import Step3 from "./Step3";
+// import Step4 from "./Step4";
 
 function Oursteps() {
   return (
