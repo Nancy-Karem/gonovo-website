8dea837ac791c8670bcb3d29edefc7df6d06de7c
alaagonovo <alaa@gonovo.com>
2025-01-21 11:58:36 +0200

    end step 3

--- src/components/ui/oursteps/Oursteps.tsx
+++ src/components/ui/oursteps/Oursteps.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import Badge from "../common/Badge";
-
 import Step1 from "./Step1";
- import Step2 from "./Step2";
-// import Step3 from "./Step3";
+import Step2 from "./Step2";
+import Step3 from "./Step3";
 // import Step4 from "./Step4";
 
 function Oursteps() {
@@ -18,8 +17,8 @@ function Oursteps() {
         <Step1 />
 
         <Step2 />
-        {/*<Step3 />
-      <Step4 /> */}
+        <Step3 />
+      {/* <Step4 />  */}
       </div>
     </section>
   );
--- src/components/ui/oursteps/Step3.tsx
+++ src/components/ui/oursteps/Step3.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./steps.module.css";
 import ImageComponent from "@/components/imagecomponent/ImageComponent";
+import Image from "next/image";
 function Step3() {
   const tech = [
     "/svgs/js.svg",
@@ -17,84 +18,88 @@ function Step3() {
     "/svgs/csharp.svg",
   ];
   return (
-    <div
-      className={`${styles.main_clip_path} relative w-fit mx-auto my-10 flex flex-col`}
-    >
-      <div className="absolute inset-0 grid grid-cols-2 px-20  items-center">
-        <div className="relative">
-          <div className={`${styles.clip} w-[403px]   bg-white relative`}>
-            <div className="pt-5 px-7 pb-2 border-b border-[#f2f2f2]">
-              <div className="flex gap-1">
-                <span className="w-3 h-3 rounded-full inline-block bg-[#999999]"></span>
-                <span className="w-3 h-3 rounded-full inline-block bg-[#C8C8C8]"></span>
-                <span className="w-3 h-3 rounded-full inline-block bg-[#DBDBDB]"></span>
+    <div className="relative w-fit xl:w-full  bg-semipurple rounded-[31px] h-auto xl:h-[443px]  sm:my-10 flex flex-col mx-auto">
+      <div className="z-10 w-[120px] h-[120px] lg:w-[150px] lg:h-[150px] rounded-es-[31px] bg-white right-[-2px] top-[-2px] absolute"></div>
+      <div
+        style={{ boxShadow: "10px -10px 0 white" }}
+        className="bg-transparent w-12 h-12 absolute lg:right-[148px] right-[118px] rounded-se-[31px]"
+      ></div>
+      <div
+        style={{ boxShadow: "10px -10px 0 white" }}
+        className="bg-transparent w-12 h-12 absolute right-[0px] lg:top-[148px] top-[118px] rounded-se-[31px]"
+      ></div>
+      <div className="grid grid-cols-1 gap-8 xl:gap-0 xl:grid-cols-2 lg:px-10 lg:py-20 px-5 py-10 xl:px-20 h-full w-fit xl:w-full  justify-center">
+        <div>
+          <div className="rounded-[12px] sm:rounded-3xl  bg-white relative w-[67%] sm:w-[70%]  xl:w-[403px] ">
+            <div className="w-[40px] h-[60px] lg:w-[85px] lg:h-[100px] rounded-es-[24px] bg-semipurple right-[-2px] top-[-2px] absolute flex items-center justify-center">
+              <div className="relative w-[27px] h-[27px] lg:w-[37.5px] lg:h-[33.75px]">
+                <Image src="/svgs/steps/angelss.svg" alt="angels code" fill />
               </div>
-              <p className="text-sm font-medium mt-6">Software Developments</p>
             </div>
-            <div className="px-8 pt-6 pb-8">
+            <div
+              style={{ boxShadow: "6px -10px 0 #aca0f8" }}
+              className="bg-transparent w-5 h-5 sm:w-12 sm:h-12 absolute lg:right-[83px] right-[38px] rounded-se-[12px]  sm:rounded-se-[24px]"
+            ></div>
+            <div
+              style={{ boxShadow: "10px -10px 0 #aca0f8" }}
+              className="bg-transparent w-5 h-5 sm:w-12 sm:h-12 absolute right-[0px] lg:top-[98px] top-[58px] rounded-se-[12px]  sm:rounded-se-[24px]"
+            ></div>
+            <div className="pt-3 px-4 sm:pt-5 sm:px-7 pb-2 border-b border-[#f2f2f2]">
+              <div className="flex gap-1">
+                <span className="w-[6px] h-[6px] sm:w-3 sm:h-3 rounded-full inline-block bg-[#999999]"></span>
+                <span className="w-[6px] h-[6px] sm:w-3 sm:h-3 rounded-full inline-block bg-[#C8C8C8]"></span>
+                <span className="w-[6px] h-[6px] sm:w-3 sm:h-3 rounded-full inline-block bg-[#DBDBDB]"></span>
+              </div>
+              <p className="text-[12px] xl:text-sm font-medium mt-3 xl:mt-6">
+                Software Developments
+              </p>
+            </div>
+            <div className="py-4 px-2 xl:p-10">
               <div className="flex gap-2 mb-2">
                 {tech.map((tech, index) => (
-                  <ImageComponent
-                    key={index}
-                    src={tech}
-                    alt=""
-                    width="30px"
-                    height="30px"
-                  />
+                  <div key={index} className="relative w-[30px] h-[30px]">
+                    <Image src={tech} alt="logo tech" fill />
+                  </div>
                 ))}
               </div>
               <div className="flex flex-wrap gap-2 max-w-28">
                 {tech2.map((tech, index) => (
-                  <ImageComponent
-                    key={index}
-                    src={tech}
-                    alt=""
-                    width="30px"
-                    height="30px"
-                  />
+                  <div key={index} className="relative w-[30px] h-[30px]">
+                    <Image src={tech} alt="logo tech" fill />
+                  </div>
                 ))}
+              </div>
+              <div
+                className="absolute -bottom-[50px] -right-[48px] sm:bottom-[-73px]
+sm:-right-[60px]"
+              >
+                <div className="relative w-[100px] h-[95px] sm:w-[143px] sm:h-[196px]">
+                  <Image src="/svgs/dotsback.svg" alt="logo tech" fill />
+                </div>
+              </div>
+              <div
+                className="absolute -bottom-[60px] -right-[40px]  sm:bottom-[-85px]
+sm:-right-[40px]"
+              >
+                <div className="relative w-[150px] h-[100px] sm:w-[237px] sm:h-[199px]">
+                  <Image
+                    src="/svgs/steps/stepimage3.svg"
+                    alt="image step3"
+                    fill
+                  />
+                </div>
               </div>
             </div>
           </div>
-          <div
-            className="absolute bottom-[-75px]
-right-[60px]"
-          >
-            <ImageComponent
-              src="/svgs/dotsback.svg"
-              alt="step1 image"
-              width="143px"
-              height="196px"
-            />
-          </div>
-          <div
-            className="absolute bottom-[-90px]
-right-[80px]"
-          >
-            <ImageComponent
-              src="/svgs/steps/stepimage3.svg"
-              alt="image step3"
-              width="237px"
-              height="199px"
-            />
-          </div>
-          <div className="absolute top-[30px] right-[150px]">
-            <ImageComponent
-              src="/svgs/steps/angelss.svg"
-              width="37.5px"
-              height="33.75px"
-              alt=""
-            />
-          </div>
         </div>
-        <div className="mt-5 w-fit ml-auto">
-          <p className="ring-2 ring-black w-fit rounded-full px-[14px] py-[2px] text-sm font-medium">
+        <div className="mt-10 sm:mt-5 w-fit ml-auto">
+          <p className="ring-2 ring-black w-fit rounded-full xl:px-[14px] xl:py-[2px] text-[12px] px-2 py-[1px] xl:text-sm font-medium">
             STEP 3
           </p>
-          <h1 className="text-3xl font-medium text-[#111111] mt-10 mb-4">
+          <h1 className="text-[24px] xl:text-3xl font-medium text-[#111111]  mt-3 sm:mt-10 mb-2 sm:mb-4">
             Development and Integration
           </h1>
-          <p className="text-darkGray leading-[23px] max-w-[460px]">
+          <p className="text-sm xl:text-base text-darkGray leading-[23px] max-w-[460px]">
             Here, we split our efforts into front-end and back-end development.
             We weave together APIs and third-party integrations to infuse the
             software with its core functionalities. This step ensures a robust
